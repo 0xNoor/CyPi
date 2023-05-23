b3326cda082d286251523cbfb1c5e3bf97ca0eed
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-23 19:45:44 +0530

    Instead of number, now it uses a bar to show the data

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import tkinter as tk
+from tkinter import ttk
 
 class SpeedometerApp(tk.Tk):
     def __init__(self, file_path):
         super().__init__()
 
         self.title("Speedometer")
-        self.geometry("400x400")
+        self.geometry("400x150")
 
-        self.speed_label = tk.Label(self, text="0", font=("Arial", 36))
-        self.speed_label.pack(pady=50)
+        self.progress_bar = ttk.Progressbar(
+            self, orient="horizontal", length=300, mode="determinate"
+        )
+        self.progress_bar.pack(pady=20)
 
         self.file_path = file_path
-        self.read_speed()
+        self.update_progress()
 
-    def read_speed(self):
+    def update_progress(self):
         try:
             with open(self.file_path, "r") as file:
                 speed = int(file.read())
-                if speed > 210:
-                    speed = 210  # Limit the maximum speed to 210
+                if speed > 220:
+                    speed = 220  # Limit the maximum speed to 220
 
-                self.speed_label.config(text=str(speed))
+                self.progress_bar["value"] = speed
 
         except FileNotFoundError:
             print("File not found.")
 
-        self.after(1000, self.read_speed)  # Read the speed every 1 second
+        self.after(1000, self.update_progress)  # Update the progress every 1 second
 
 if __name__ == "__main__":
     app = SpeedometerApp("speed_data.txt")
