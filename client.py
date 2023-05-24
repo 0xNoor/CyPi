6d84eeead844a9ff8498cd2cd07f72280536b664
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-24 19:26:11 +0530

    A basic template for client-server communication

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = 'server_ip_address'  # IP address of the server
+PORT = 12345  # Port number to connect to
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((HOST, PORT))
+
+while True:
+    # Get command input from the user
+    command = input("Enter command ('hello', 'quit'): ")
+
+    # Send the command to the server
+    client_socket.send(command.encode())
+
+    if command == "quit":
+        break
+
+# Close the client socket
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+
+HOST = 'your_ip_address'  # IP address of the server
+PORT = 12345  # Port number to listen on
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the host and port
+server_socket.bind((HOST, PORT))
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print("Server listening on {}:{}".format(HOST, PORT))
+
+# Accept a client connection
+client_socket, client_address = server_socket.accept()
+print("Client connected:", client_address)
+
+while True:
+    # Receive command from the client
+    command = client_socket.recv(1024).decode()
+
+    # Process the command
+    if command == "hello":
+        print("Command received: hello")
+        # Add your code here to perform the corresponding action for the command
+
+    elif command == "quit":
+        print("Command received: quit")
+        break
+
+# Close the client socket and server socket
+client_socket.close()
+server_socket.close()
