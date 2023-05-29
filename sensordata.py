22285eae734efc41759233cedd620cfbc66f30a6
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-29 18:56:03 +0530

    added implementation of MPU6050

--- sensordata.py
+++ sensordata.py
@@ -76,4 +76,14 @@ def get_mpu6050_data(i2c):
     }
 
 
+i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=400000)
+init_mpu6050(i2c)
+
+
 read_signals()
+while True:
+    data = get_mpu6050_data(i2c)
+    print("Temperature: {:.2f} °C".format(data['temp']))
+    print("Acceleration: X: {:.2f}, Y: {:.2f}, Z: {:.2f} g".format(data['accel']['x'], data['accel']['y'], data['accel']['z']))
+    print("Gyroscope: X: {:.2f}, Y: {:.2f}, Z: {:.2f} °/s".format(data['gyro']['x'],data['gyro']['y'], data['gyro']['z']))
+    utime.sleep(0.5)
