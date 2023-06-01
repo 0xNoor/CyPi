ec5dc4188afb63c40260264875d8e8657fb181e9
Mohd Noor Aman <nooraman5718@gmail.com>
2023-06-01 21:22:49 +0530

    added the speech recognition to server-client method

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import os
+import speech_recognition as sr
+import time
+import sounddevice
+import pyautogui
 
 HOST = '0.0.0.0'  # IP address of the server
 PORT = 12345  # Port number to listen on
@@ -26,7 +30,35 @@ while True:
     # Process the command
     if command == "hello":
         print("Command received: hello")
-        os.system("python sensordata.py")
+        r = sr.Recognizer()
+
+    # Specify the audio source (e.g., microphone or audio file)
+    # For microphone input, use sr.Microphone() as the source
+    # For audio file input, use sr.AudioFile('audio_file.wav') as the source
+        source = sr.Microphone(device_index=1)
+
+    # Adjust for ambient noise (optional)
+    # Uncomment the following line if you need to adjust for ambient noise
+    # with source as source:
+
+
+        print("Listening...")
+
+        with source as source:
+            audio = r.listen(source, phrase_time_limit=5)
+
+    # Perform speech recognition
+        try:
+        # Use the default API key for Google Speech Recognition
+            print("test")
+            text = r.recognize_google(audio)
+            print("Recognized Text:")
+            pyautogui.typewrite(text)
+        except sr.UnknownValueError:
+            print("Speech recognition could not understand audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+
         # Add your code here to perform the corresponding action for the command
 
     elif command == "quit":
