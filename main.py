eaf4cc6e0861fdbfbac63afbd0ed51e3ca689665
Mohd Noor Aman <nooraman5718@gmail.com>
2023-06-16 22:33:59 +0530

    Created a menu which can be used to trigger different function

--- main.py
+++ main.py
@@ -30,6 +30,53 @@ class SpeedometerApp(tk.Tk):
 
         self.after(1000, self.update_progress)  # Update the progress every 1 second
 
-if __name__ == "__main__":
+
+def open_speedometer():
     app = SpeedometerApp("speed_data.txt")
     app.mainloop()
+
+def function1():
+    print("Function 1 executed")
+
+#More functions can be added here
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    # Create a 3x3 grid
+    for i in range(3):
+        root.columnconfigure(i, weight=1, minsize=100)
+        root.rowconfigure(i, weight=1, minsize=100)
+
+        for j in range(3):
+            button = ttk.Button(
+                root,
+                text=f"Button {i*3+j+1}",
+                command=lambda i=i, j=j: button_click(i, j),
+            )
+            button.grid(row=i, column=j, padx=10, pady=10)
+
+    def button_click(i, j):
+        # Define the actions for each button
+        if i == 0 and j == 0:
+            open_speedometer()
+        elif i == 0 and j == 1:
+            function1()
+        elif i == 0 and j == 2:
+            function2()
+        elif i == 1 and j == 0:
+            function3()
+        elif i == 1 and j == 1:
+            # Add actions for other buttons here
+            pass
+        elif i == 1 and j == 2:
+            pass
+        elif i == 2 and j == 0:
+            pass
+        elif i == 2 and j == 1:
+            pass
+        elif i == 2 and j == 2:
+            pass
+
+    root.mainloop()
